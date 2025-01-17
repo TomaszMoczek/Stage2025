8323730ffc247834db254a62c1eec0578386cddc
Tomasz Moczek <tomasz.moczek@kantar.com>
2025-01-17 15:37:04 +0100

    changes added

--- Python/audio_certification_sequence_extractor.py
+++ Python/audio_certification_sequence_extractor.py
@@ -1,8 +1,28 @@
+import os
 import sys
+import numpy
+import scipy
 
 
 def main() -> int:
-    print("Hello World!")
+    file_names = (
+        "../Captures/raw/CTCE7_65dB10cm60dB30cm60dB2m.wav",
+        "../Captures/raw/CTCELC2_10cmVol9_2.wav",
+        "../Captures/raw/CTCELCWG2_1_60dB2m.wav",
+        "../Captures/raw/CTCELCWG2_1_65dB10cm60dB30cm_mic1.wav",
+        "../Captures/raw/CTCELCWG2_1_lineIn_VOL50.wav",
+        "../KantarCertificationMeters.wav",
+    )
+
+    for i in range(len(file_names)):
+        file_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            file_names[i],
+        )
+        fs, data = scipy.io.wavfile.read(file_path)
+        data = numpy.vstack((data,))
+        print(file_path, fs, data.shape, len(data))
+
     return 0
 
 
