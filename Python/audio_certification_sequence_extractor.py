e344abcac8ae540ab73da149b1d96b6b8e2496bd
Tomasz Moczek <tomasz.moczek@kantar.com>
2025-01-22 18:13:19 +0100

    changes added

--- Python/audio_certification_sequence_extractor.py
+++ Python/audio_certification_sequence_extractor.py
@@ -3,26 +3,50 @@ import sys
 import numpy
 import scipy
 
+from dsp_plotter import DspPlotter
+
+
+def usage() -> None:
+    print(
+        "Usage: python3 ./audio_certification_sequence_extractor.py [-h | -i input *.wav file's path [-o output folder's path | -p [-s]]]"
+    )
+    print()
+    print("         -h                          - display this help's message")
+    print(
+        "         -i input *.wav file's path  - path of the input *.wav file [mandatory]"
+    )
+    print(
+        "         -o output folder's path     - path of the output folder for the extracted *.wav file(-s) [default is a path of the input *.wav file's parent folder]"
+    )
+    print("         -p                          - plot of the input *.wav file's data")
+    print(
+        "         -s                          - spectrogram of the input *.wav file's data"
+    )
+    print()
+
+
+def parse_file(input_file_path) -> None:
+    pass
+
+
+def plot_file(input_file_path) -> None:
+    file_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        input_file_path,
+    )
+    if os.path.isfile(file_path):
+        fs, data = scipy.io.wavfile.read(file_path)
+        data = numpy.vstack((data,)) if len(data.shape) == 1 else data.T
+        print(file_path, fs, len(data), data.dtype)
+
 
 def main() -> int:
-    file_names = (
-        "../Captures/raw/CTCE7_65dB10cm60dB30cm60dB2m.wav",
-        "../Captures/raw/CTCELC2_10cmVol9_2.wav",
-        "../Captures/raw/CTCELCWG2_1_60dB2m.wav",
-        "../Captures/raw/CTCELCWG2_1_65dB10cm60dB30cm_mic1.wav",
-        "../Captures/raw/CTCELCWG2_1_lineIn_VOL50.wav",
-        "../KantarCertificationMeters.wav",
-    )
+    print("audio_certification_sequence_extractor v1.0")
+    print("Copyright \u00A9 2025, Kantar Media")
+    print()
 
-    for i in range(len(file_names)):
-        file_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            file_names[i],
-        )
-        if os.path.isfile(file_path):
-            fs, data = scipy.io.wavfile.read(file_path)
-            data = numpy.vstack((data,)) if len(data.shape) == 1 else data.T
-            print(file_path, fs, len(data), data.dtype)
+    if len(sys.argv) == 1:
+        usage()
 
     return 0
 
--- Python/test_audio_certification_sequence_extractor.py
+++ Python/test_audio_certification_sequence_extractor.py
@@ -1,5 +1,19 @@
 import audio_certification_sequence_extractor
 
 
+def test_usage() -> None:
+    audio_certification_sequence_extractor.usage()
+
+
+def test_parse_file() -> None:
+    audio_certification_sequence_extractor.parse_file(
+        "../KantarCertificationMeters.wav"
+    )
+
+
+def test_plot_file() -> None:
+    audio_certification_sequence_extractor.plot_file("../KantarCertificationMeters.wav")
+
+
 def test_main() -> None:
     assert audio_certification_sequence_extractor.main() == 0
