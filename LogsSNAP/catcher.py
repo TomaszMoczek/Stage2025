9efac1021157b5d17207aa996272186e78976fca
Tomasz Moczek <tomasz.moczek@kantar.com>
2025-01-30 13:35:56 +0100

    changes added

--- LogsSNAP/catcher.py
+++ LogsSNAP/catcher.py
@@ -1,5 +1,6 @@
 import os
-import pandas as pd
+import numpy
+import pandas
 
 file_names = [
     "CompactDetectionLog-CTCE7_65dB10cm60dB30cm60dB2m.wav.txt",
@@ -21,95 +22,50 @@ for i in range(len(file_names)):
         print(file_path, "file likely does not exist")
         continue
 
-    print()
     print(file_path)
     print()
 
-    df = pd.read_csv(
+    df = pandas.read_csv(
         filepath_or_buffer=file_path,
         sep="\t",
         header=None,
     )
-    # df.fillna(value=0.0, inplace=True)
+    df.fillna(value=0.0, inplace=True)
     df.columns = ["begin", "end", "type", "id", "timestamp"]
+    df: pandas.DataFrame = df.loc[df["id"] == "00002C48"]
 
     print(df)
     print()
 
-    indices_of_sequences = []
-    for i, x in enumerate(df["ID"]):
-        if x == "00002C48":  # searching for the right ID
-            indices_of_sequences.append(i)
+    flag = False
+    begin_timestamps = []
+    TOTAL_SEQUENCE_LENGTH = 1107.0
 
-    print(indices_of_sequences)
-    print()
-
-    starts = []
-    for index, i in enumerate(indices_of_sequences):
-        # if watermark in one line
-        length = (
-            df.loc[i, "end"] - df.loc[i, "start"]
-        )  # checking the length of watermark
-        if (
-            length >= 1107
-        ):  # how to tune it? As we discussed last week, the tool is not very precise at the end, so this is a value that fits to files from You,
-            # but may not in general
-            start = (
-                df.loc[i, "start"] - 90 - df.loc[i, "TST"]
-            )  # finding the start of the sequence
-            starts.append(start)
-        else:
-            if index > 0:
-                # if watermark in two lines
-                if indices_of_sequences[index] - indices_of_sequences[index - 1] == 1:
-                    length = df.loc[i, "end"] - df.loc[i - 1, "start"]
-                    if length >= 1107:
-                        start = df.loc[i - 1, "start"] - 90 - df.loc[i - 1, "TST"]
-                        starts.append(start)
-                    else:  # if watermark in three ore more lines
-                        counter = index
-                        while length < 1107:
-                            length = (
-                                df.loc[indices_of_sequences[counter], "end"]
-                                - df.loc[i - 1, "start"]
-                            )
-                            counter += 1
-                        start = df.loc[i - 1, "start"] - 90 - df.loc[i - 1, "TST"]
-                        starts.append(start)
-                        break  # not the solution, I need to jump out of loop and iterate above counter (maybe recursion?)
-
-    print(starts)
-    print()
-
-    # idea based on TST - not working yet
-    sequence = []
-    sequences = []
-    for index, i in enumerate(df["ID"]):
-        if i == "00002C48":
-            length = (
-                df.loc[index, "end"] - df.loc[index, "start"]
-            )  # checking the length of watermark
-            if length >= 1107:
-                sequence.append(index)
-                sequences.append(sequence)
-                sequence = []
+    for j in range(len(df)):
+        if not flag:
+            begin = df.iloc[j]["begin"]
+            timestamp = df.iloc[j]["timestamp"]
+            duration = df.iloc[j]["end"] - begin
+            if duration >= TOTAL_SEQUENCE_LENGTH:
+                # watermark in a single line
+                begin_timestamp = begin - 90.0 - timestamp
+                if begin_timestamp < 0.0:
+                    begin_timestamp = numpy.float64(0.0)
+                begin_timestamps.append(begin_timestamp)
             else:
-                if df.loc[index + 1, "start"] - df.loc[index, "end"] < 100:
-                    if df.loc[index, "TST"] < df.loc[index + 1, "TST"]:
-                        length = df.loc[index + 1, "end"] - df.loc[index, "start"]
-                        if length >= 1107:
-                            sequence.append(index)
-                            sequences.append(sequence)
-                            sequence = []
-                        else:  # if watermark in three ore more lines
-                            counter = index
-                            while length < 1107:
-                                length = (
-                                    df.loc[counter, "end"] - df.loc[index - 1, "start"]
-                                )
-                                counter += 1
+                # watermark in more than just one line
+                flag = True
+        else:
+            # watermark in more than just one line
+            duration += df.iloc[j]["end"] - begin
+            if duration >= TOTAL_SEQUENCE_LENGTH:
+                begin_timestamp = begin - 90.0 - timestamp
+                if begin_timestamp < 0.0:
+                    begin_timestamp = numpy.float64(0.0)
+                begin_timestamps.append(begin_timestamp)
+                flag = False
 
-    print(sequences)
+    print(begin_timestamps)
     print()
 
 # here is what I expect without subtraction of the offset, but I manualy checked, that it is necessery to substract it
