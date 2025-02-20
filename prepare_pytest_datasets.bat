d1ab586e7df81980e1b6df9f71a198b2f209e674
Tomasz Moczek <tomasz.moczek@kantar.com>
2025-02-20 12:12:46 +0100

    changes added

--- prepare_pytest_datasets.bat
+++ prepare_pytest_datasets.bat
@@ -1,17 +1,14 @@
 set repo=Captures\raw
 
-
-REM -------------------- SNAP ----------------------------------
-
 call SamplesAudioWM\Sample_reader.exe -i KantarCertificationMeters.wav -type SNAP -profile P5T
 ren CompactDetectionLog.txt CompactDetectionLog-KantarCertificationMeters.wav.txt
-ren flog.txt flog-KantarCertificationMeters.wav.txt
+del flog.txt
 del *.dat
 
 for %%i in (%repo%\*.wav) do (
 call SamplesAudioWM\Sample_reader.exe -i %%i -type SNAP -profile P5T
 ren CompactDetectionLog.txt CompactDetectionLog-%%~nxi.txt
-ren flog.txt flog-%%~nxi.txt
+del flog.txt
 del *.dat
 )
 
